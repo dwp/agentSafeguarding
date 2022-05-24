9a08b7212220d9852788e17066e8186d0b11646a
timcooper-DWP <tim.cooper@engineering.digital.dwp.gov.uk>
2022-05-24 14:48:25 +0100

    Add incident info amends to routing, content and errors

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,44 @@
 const express = require('express')
 const router = express.Router()
 
+
+// Run this code when a form is submitted to 'test' asking for ref number only if the police were called
+router.post('/beta/incident_manager/test', function (req, res) {
+
+    // Make a variable and give it the value from 'prnQuestion'
+    var werePoliceCalled = req.session.data['policeQuestion']
+  
+    // Check whether the variable matches a condition
+    if (werePoliceCalled == "no"){
+      // Send user to next page
+      res.redirect('/beta/incident_manager/04add_soars')
+    } else {
+      // Send user to ineligible page
+      res.redirect('/beta/incident_manager/02action')
+    }
+  
+  })
+
+// Run this code when a form is submitted to 'test2' asking if reported in SOARS
+router.post('/beta/incident_manager/test2', function (req, res) {
+
+    // Make a variable and give it the value from 'prnQuestion'
+    var werePoliceCalled = req.session.data['soarsQuestion']
+  
+    // Check whether the variable matches a condition
+    if (werePoliceCalled == "no"){
+      // Send user to next page
+      res.redirect('end')
+    } else {
+      // Send user to ineligible page
+      res.redirect('/beta/incident_manager/05fast')
+    }
+  
+  })
+
+
+
+
 // Add your routes here - above the module.exports line
 
 // copy the const name and the file path to new version
