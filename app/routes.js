cfdcc01924432d4908161cab0566c16216c503d2
timcooper-DWP <103037700+timcooper-DWP@users.noreply.github.com>
2022-10-07 15:58:23 +0100

    Refactor Incident report journey

--- app/routes.js
+++ app/routes.js
@@ -275,6 +275,23 @@ router.post('/beta/report_incident/reportv9/route_incident', function (req, res)
       // Send user to ineligible page
       res.redirect('01report_error')
     }
+})
+
+// Route incident or concern
+router.post('/beta/report_incident/reportv10/route_incident', function (req, res) {
+  // Make a variable and give it the value from 'prnQuestion'
+  var reportType = req.session.data['report-type']
+    // Check whether the variable matches a condition
+    if (reportType == "incident") {
+      // Send user to next page
+      res.redirect('02behalf')
+    } else if (reportType == "concern") {
+      // Send user to next page
+      res.redirect('concern/00agentsource')
+    } else {
+      // Send user to ineligible page
+      res.redirect('01report_error')
+    }
 
 })
 
